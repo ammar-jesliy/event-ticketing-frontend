6273bca6d483909133461712334d5730d20f165c
ammar-jesliy <ammarjc1@gmail.com>
2024-10-21 19:05:23 +0530

    Set up routing, create Dashboard route and page not found route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+];
