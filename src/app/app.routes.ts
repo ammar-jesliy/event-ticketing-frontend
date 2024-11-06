86f549f58ad6f4aaa580ed4aa1b6aad34ca4155e
ammar-jesliy <ammarjc1@gmail.com>
2024-11-06 16:40:21 +0530

    Add auth guard for dashboard and redirect to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
-    {path: 'dashboard', component: DashboardComponent},
+    {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
     {path: '**', component: PageNotFoundComponent}
 ];
