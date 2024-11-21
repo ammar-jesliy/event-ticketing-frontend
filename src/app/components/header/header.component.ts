c92c4cad39338097c32a645cba27b169947d07d2
ammar-jesliy <ammarjc1@gmail.com>
2024-11-21 17:31:25 +0530

    Style layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { SidemenuComponent } from "../sidemenu/sidemenu.component";
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  title: String = '';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.title =
+          this.router.url.split('/')[1].charAt(0).toUpperCase() +
+          this.router.url.split('/')[1].slice(1);
+      }
+    });
+  }
 }
