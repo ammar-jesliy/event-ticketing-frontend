f8da7f8f2132a774657089db984f9ba438f5af60
ammar-jesliy <ammarjc1@gmail.com>
2024-11-21 20:55:44 +0530

    Create Dashboard tickets sold, earnings, and customers  analytic cards

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,24 +2,30 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { HomeTemplateComponent } from "../home-template/home-template.component";
+import { HomeTemplateComponent } from '../home-template/home-template.component';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, ButtonModule, RouterModule, HomeTemplateComponent],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    RouterModule,
+    HomeTemplateComponent,
+    TooltipModule,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-
   constructor(private router: Router) {}
 
   logout() {
-    console.log("Logged Out")
+    console.log('Logged Out');
 
-    localStorage.removeItem("isAuthenticated");
-    localStorage.removeItem("userRole");
+    localStorage.removeItem('isAuthenticated');
+    localStorage.removeItem('userRole');
     this.router.navigate(['/login']);
   }
 }
