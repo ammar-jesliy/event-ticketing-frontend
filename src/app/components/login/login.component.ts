7d68874feced0f2f7f410e9adca54a0642a7a979
ammar-jesliy <ammarjc1@gmail.com>
2024-11-24 12:05:40 +0530

    feat: add admin login logic
    hardcoded admin login logic

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -31,8 +31,12 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-
   loading: boolean = false;
+
+  private adminCredentials = {
+    email: 'admin@admin.com',
+    password: 'admin123',
+  }
 
   constructor(
     private fb: FormBuilder,
@@ -53,7 +57,23 @@ export class LoginComponent implements OnInit {
       this.loading = true;
       const { email, password } = this.loginForm.value;
 
-      // Check email in both customer and vendor services
+      // Check if the user is an admin
+      if (email === this.adminCredentials.email && password === this.adminCredentials.password) {
+        this.loading = false;
+
+        // Store user authentication status
+        localStorage.setItem('isAuthenticated', 'true');
+        localStorage.setItem('userRole', 'admin');
+        localStorage.setItem('user', JSON.stringify({ email: email }));
+
+        alert('Successfully logged in as admin');
+        this.loginForm.reset();
+
+        this.router.navigate(['/dashboard']);
+        return;
+      }
+
+      // Non-admin logic: Check email in both customer and vendor services
       forkJoin({
         isCustomer: this.customerService.checkEmailAvailability(email),
         isVendor: this.vendorService.checkEmailAvailability(email),
