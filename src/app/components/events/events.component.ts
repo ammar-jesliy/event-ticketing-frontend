63add2f6592e663e30c47c2c07bc96d1d118227c
ammar-jesliy <ammarjc1@gmail.com>
2024-11-27 16:51:03 +0530

    feat: create add event form ui
    creates a form to add events, no validation yet

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
-import { SidemenuComponent } from '../sidemenu/sidemenu.component';
-import { HeaderComponent } from '../header/header.component';
-import { HomeTemplateComponent } from "../home-template/home-template.component";
+import { HomeTemplateComponent } from '../home-template/home-template.component';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CalendarModule } from 'primeng/calendar';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [HomeTemplateComponent],
+  imports: [
+    HomeTemplateComponent,
+    DialogModule,
+    InputTextModule,
+    ButtonModule,
+    InputTextareaModule,
+    CalendarModule,
+    CommonModule,
+    FormsModule,
+    InputNumberModule,
+  ],
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css'
+  styleUrl: './events.component.css',
 })
 export class EventsComponent {
+  date: Date | undefined;
+  formVisible: boolean = false;
 
+  showDialog() {
+    this.formVisible = true;
+    console.log('Show Dialog');
+  }
 }
