5958b1abeb8a1057752226cd80a857aa666bf1c7
ammar-jesliy <ammarjc1@gmail.com>
2024-11-29 18:08:39 +0530

    feat: add openDate and closeDate to Event model

--- src/app/util/event.ts
+++ src/app/util/event.ts
@@ -1,9 +1,11 @@
 export interface Event {
-    id?: string;
-    name: string;
-    description: string;
-    date: any;
-    location: string;
-    maxCapacity: number;
-    eventStatus?: string;  
+  id?: string;
+  name: string;
+  description: string;
+  date: string;
+  openDate: string;
+  closeDate: string;
+  location: string;
+  maxCapacity: number;
+  eventStatus?: string;
 }
