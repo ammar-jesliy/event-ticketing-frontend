386e8ab0ed7b1ed5c844e3aed2b0265fce548863
ammar-jesliy <ammarjc1@gmail.com>
2024-12-04 18:59:55 +0530

    feat: add transaction type interface

---
+++ src/app/util/transaction.ts
@@ -0,0 +1,12 @@
+export interface Transaction{
+    id?: string;
+    timeStamp: string;
+    ticketIds: string[];
+    customerId?: string;
+    vendorId?: string;
+    eventId: string;
+    quantity: number;
+    pricePerTicket: number;
+    totalAmount?: number;
+    transactionType: string;
+}
