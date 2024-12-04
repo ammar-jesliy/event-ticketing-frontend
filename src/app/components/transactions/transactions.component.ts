7acc96dcf989bdabaf39fc3b22ee6b8beced47fb
ammar-jesliy <ammarjc1@gmail.com>
2024-12-04 20:43:07 +0530

    feat: display transactions in a table
    accesses the transactions signal to display the transactions in a table

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { HomeTemplateComponent } from "../home-template/home-template.component";
+import { Component, OnInit, Signal } from '@angular/core';
+import { HomeTemplateComponent } from '../home-template/home-template.component';
+import { CommonModule } from '@angular/common';
+import { TooltipModule } from 'primeng/tooltip';
+import { Transaction } from '../../util/transaction';
+import { Event } from '../../util/event';
+import { TransactionService } from '../../services/transaction.service';
+import { EventService } from '../../services/event.service';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [HomeTemplateComponent],
+  imports: [HomeTemplateComponent, CommonModule, TooltipModule],
   templateUrl: './transactions.component.html',
-  styleUrl: './transactions.component.css'
+  styleUrl: './transactions.component.css',
 })
-export class TransactionsComponent {
+export class TransactionsComponent implements OnInit {
+  allEvents: Signal<Event[] | null>;
+  vendorTransactions: Signal<Transaction[] | null>;
 
+  constructor(
+    private transactionService: TransactionService,
+    private eventService: EventService
+  ) {
+    this.allEvents = this.eventService.allEvents;
+    this.vendorTransactions = this.transactionService.vendorTransactions;
+  }
+
+  ngOnInit(): void {
+    this.eventService.fetchAllEvents();
+    this.transactionService.fetchTransactionsByVendorId(
+      JSON.parse(localStorage.getItem('user') || '{}').id
+    );
+  }
+
+  getEventName(eventId: string) {
+    return this.allEvents()?.find((event) => event.id === eventId)?.name || '';
+  }
 }
