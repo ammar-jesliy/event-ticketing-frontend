8835089a9f1820b4a6f422c1004753ed53cb27b1
ammar-jesliy <ammarjc1@gmail.com>
2024-12-07 12:18:31 +0530

    feat: add dashboard data to admin dashboard
    adds total tickets sold
    adds total revenue
    adds total users

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal } from '@angular/core';
 import { HomeTemplateComponent } from '../home-template/home-template.component';
+import { Ticketpool } from '../../util/ticketpool';
+import { TicketpoolService } from '../../services/ticketpool.service';
+import { TooltipModule } from 'primeng/tooltip';
+import { CommonModule } from '@angular/common';
+import { TicketService } from '../../services/ticket.service';
+import { Ticket } from '../../util/ticket';
+import { Customer } from '../../util/customer';
+import { CustomerService } from '../../services/customer.service';
+import { Vendor } from '../../util/vendor';
+import { VendorService } from '../../services/vendor.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [HomeTemplateComponent],
+  imports: [HomeTemplateComponent, TooltipModule, CommonModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css',
 })
-export class AdminDashboardComponent {}
+export class AdminDashboardComponent implements OnInit {
+  allTicketPools: Signal<Ticketpool[] | null>;
+  allTickets: Signal<Ticket[] | null>;
+  allCustmomers: Signal<Customer[] | null>;
+  allVendors: Signal<Vendor[] | null>;
+
+  constructor(
+    private ticketpoolService: TicketpoolService,
+    private ticketService: TicketService,
+    private customerService: CustomerService,
+    private vendorService: VendorService
+  ) {
+    this.allTicketPools = this.ticketpoolService.allTicketPools;
+    this.allTickets = this.ticketService.allTickets;
+    this.allCustmomers = this.customerService.allCustomers;
+    this.allVendors = this.vendorService.allVendors;
+  }
+
+  ngOnInit(): void {
+    this.ticketpoolService.fetchAllTicketPools();
+    this.ticketService.fetchAllTickets();
+    this.customerService.fetchAllCustomers();
+  }
+
+  // Get the totoal number of sold tickets in all ticket pools
+  getTotalSoldTickets(): number {
+    const ticketPools = this.allTicketPools();
+    console.log(this.allTicketPools());
+    if (!ticketPools) {
+      return 0;
+    }
+    return ticketPools.reduce(
+      (acc, ticketPool) => acc + ticketPool.ticketSold,
+      0
+    );
+  }
+
+  // Get the total price of all tickets in the allTickets signal array with available as false
+  getTotalRevenue(): number {
+    const tickets = this.allTickets();
+    if (!tickets) {
+      return 0;
+    }
+    return tickets.reduce(
+      (acc, ticket) => (ticket.available ? acc : acc + ticket.price),
+      0
+    );
+  }
+
+  // Get the total number of users in the allCustomers and allVendors signal arrays
+  getTotalUsers(): number {
+    const customers = this.allCustmomers();
+    const vendors = this.allVendors();
+    const customerCount = customers ? customers.length : 0;
+    const vendorCount = vendors ? vendors.length : 0;
+    return customerCount + vendorCount;
+  }
+}
--- src/app/services/ticketpool.service.ts
+++ src/app/services/ticketpool.service.ts
@@ -1,15 +1,29 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, signal, Signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Ticketpool } from '../util/ticketpool';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TicketpoolService {
   apiUrl = 'http://localhost:8080/api/v1/ticketpools';
 
-  constructor(private http: HttpClient) { }
+  private _allTicketPools = signal<Ticketpool[] | null>(null);
+
+  get allTicketPools() {
+    return this._allTicketPools.asReadonly();
+  }
+
+  constructor(private http: HttpClient) {}
+
+  fetchAllTicketPools() {
+    return this.http
+      .get<Ticketpool[]>(`${this.apiUrl}`)
+      .subscribe((ticketPools) => {
+        this._allTicketPools.set(ticketPools);
+      });
+  }
 
   fetchTicketPoolByEventId(eventId: string) {
     return this.http.get<Ticketpool>(`${this.apiUrl}/${eventId}`);
