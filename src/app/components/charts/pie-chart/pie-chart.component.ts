b9f5f42bdcae5651a4d7c7ec45f144baf8c76b1d
ammar-jesliy <ammarjc1@gmail.com>
2024-12-07 15:40:58 +0530

    feat: create pie chart component
    creates a basic pie chart component that can be used to display 2 values in a pie chart format

---
+++ src/app/components/charts/pie-chart/pie-chart.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pie-chart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css'],
+})
+export class PieChartComponent implements OnInit {
+  @Input() value1: number = 50; // Default value
+  @Input() value2: number = 50; // Default value
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.value1);
+  }
+
+  getRotation(value1: number, value2: number): number {
+    return (value1 / (value1 + value2)) * 180 - 90;
+  }
+}
