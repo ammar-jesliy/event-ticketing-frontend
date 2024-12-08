83c20b416996d3a874542713198c0c9f94e5ac4f
ammar-jesliy <ammarjc1@gmail.com>
2024-12-08 09:46:49 +0530

    feat: enhance dashboard with ticket summary and active events components

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, Signal } from '@angular/core';
+import { Component, OnInit, Signal } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { HomeTemplateComponent } from '../home-template/home-template.component';
 import { TooltipModule } from 'primeng/tooltip';
 import { VendorService } from '../../services/vendor.service';
 import { Vendor } from '../../util/vendor';
-import { Customer } from '../../util/customer';
-import { CustomerService } from '../../services/customer.service';
+import { Ticket } from '../../util/ticket';
+import { TicketService } from '../../services/ticket.service';
+import { PieChartComponent } from '../charts/pie-chart/pie-chart.component';
+import { TransactionService } from '../../services/transaction.service';
+import { Transaction } from '../../util/transaction';
+import { EventService } from '../../services/event.service';
+import { Event } from '../../util/event';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,38 +23,120 @@ import { CustomerService } from '../../services/customer.service';
     RouterModule,
     HomeTemplateComponent,
     TooltipModule,
+    PieChartComponent,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   vendorDetails: Signal<Vendor | null>;
-  customerDetails: Signal<Customer | null>;
+  vendorTickets: Signal<Ticket[] | null>;
+  vendorTransactions: Signal<Transaction[] | null>;
+  allEvents: Signal<Event[] | null>;
 
   constructor(
-    private router: Router,
     private vendorService: VendorService,
-    private customerService: CustomerService
+    private ticketService: TicketService,
+    private transactionService: TransactionService,
+    private eventService: EventService
   ) {
     this.vendorDetails = this.vendorService.vendorDetails;
-    this.customerDetails = this.customerService.customerDetails;
+    this.vendorTickets = this.ticketService.vendorTickets;
+    this.vendorTransactions = this.transactionService.vendorTransactions;
+    this.allEvents = this.eventService.allEvents;
   }
 
   ngOnInit() {
-    const role = localStorage.getItem('userRole');
-
-    if (role === 'customer') {
-      this.customerService.loadCustomerFromStorage();
-    } else if (role === 'vendor') {
-      this.vendorService.loadVendorFromStorage();
-    }
+    this.vendorService.loadVendorFromStorage();
+    this.ticketService.fetchTicketsByVendorId(this.vendorDetails()?.id || '');
+    this.transactionService.fetchTransactionsByVendorId(
+      this.vendorDetails()?.id || ''
+    );
+    this.eventService.fetchAllEvents();
   }
 
-  logout() {
-    console.log('Logged Out');
+  // Calculate the total tickets with available as false
+  getTotalSoldTickets(): number {
+    return (
+      this.vendorTickets()?.filter((ticket) => !ticket.available).length || 0
+    );
+  }
 
-    localStorage.removeItem('isAuthenticated');
-    localStorage.removeItem('userRole');
-    this.router.navigate(['/login']);
+  // Calculate the total tickets with available as true
+  getTotalAvailableTickets(): number {
+    return (
+      this.vendorTickets()?.filter((ticket) => ticket.available).length || 0
+    );
+  }
+
+  // Calculate the total price of all sold tickets
+  getTotalRevenue(): number {
+    return (
+      this.vendorTickets()?.reduce((total, ticket) => {
+        if (!ticket.available) {
+          return total + ticket.price;
+        }
+        return total;
+      }, 0) || 0
+    );
+  }
+
+  // Calculate the total number of unique customer who bought tickets using the customer ids in each ticket with available as false
+  getTotalCustomers(): number {
+    return (
+      this.vendorTickets()?.reduce((customers, ticket) => {
+        if (
+          !ticket.available &&
+          ticket.customerId &&
+          !customers.includes(ticket.customerId)
+        ) {
+          customers.push(ticket.customerId);
+        }
+        return customers;
+      }, [] as string[]).length || 0
+    );
+  }
+
+  // Return the transactions with transactionType = "RELEASE" in the vendorTransactions signal array, sort by date in descending order and return the first 4 transactions
+  getRecentTransactions(): Transaction[] {
+    return (
+      this.vendorTransactions()
+        ?.filter((transaction) => transaction.transactionType === 'RELEASE')
+        .sort((a, b) => {
+          return (
+            new Date(b.timeStamp).getTime() - new Date(a.timeStamp).getTime()
+          );
+        })
+        .slice(0, 4) || []
+    );
+  }
+
+  // Get the total number of tickets with isAvailable = false for a given eventId
+  getSoldTicketsCount(eventId: string): number {
+    return (
+      this.vendorTickets()?.filter(
+        (ticket) => ticket.eventId === eventId && !ticket.available
+      ).length || 0
+    );
+  }
+
+  // Get the percentage of tickets sold for an event release, divide the number of sold tickets by the number of tickets released for the event from the transactions amount, take eventId and transactionId as parameter ang get sold tickets from the getSoldTicketsCount method
+  getPercentageSold(eventId: string, transactionId: string): number {
+    const totalTickets = this.vendorTransactions()?.find(
+      (transaction) => transaction.id === transactionId
+    )?.quantity;
+
+    const soldTickets = this.getSoldTicketsCount(eventId);
+    return (soldTickets / totalTickets!) * 100;
+  }
+
+  // Get the event name by eventId
+  getEventName(eventId: string): string {
+    return this.allEvents()?.find((event) => event.id === eventId)?.name || '';
+  }
+
+  // Get the event date by eventId
+  getEventDate(eventId: string): string {
+    return this.allEvents()?.find((event) => event.id === eventId)?.date || '';
   }
 }
