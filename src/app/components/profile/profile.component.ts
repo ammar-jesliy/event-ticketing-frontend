b637a33742e01a37e43a02457e7c11d7c091ce69
ammar-jesliy <ammarjc1@gmail.com>
2024-12-11 16:49:39 +0530

    feat: add documentation

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,3 +1,10 @@
+/**
+ * ProfileComponent is responsible for displaying and managing the user's profile information.
+ * It retrieves user details from local storage and fetches customer details from the server.
+ * The component also provides a method to calculate the VIP points percentage.
+ *
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { HomeTemplateComponent } from '../home-template/home-template.component';
 import { TooltipModule } from 'primeng/tooltip';
@@ -16,9 +23,16 @@ export class ProfileComponent implements OnInit {
   userDetails: any;
   customerDetails: Customer | null = null;
 
-  constructor(private customerService: CustomerService) {
-  }
+  constructor(private customerService: CustomerService) {}
 
+  /**
+   * Lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
+   * This method is used to retrieve user details from local storage and fetch customer details from the server.
+   *
+   * - Retrieves user details from local storage and parses them into an object.
+   * - Calls the customer service to fetch customer details by user ID.
+   * - Subscribes to the customer service observable to receive customer data and assigns it to `customerDetails`.
+   */
   ngOnInit() {
     this.userDetails = JSON.parse(localStorage.getItem('user') || '{}');
     this.customerService
@@ -29,14 +43,23 @@ export class ProfileComponent implements OnInit {
       });
   }
 
-  // Take the vip points as input and return the percentage of how much points are past the previous level. There are 3 levels, 0-100, 101-200, 201-300 and then 300+ is the max level. If the points are 150, then the percentage is 50% because it is 50% past the 100 level. If the points are over 300, then the percentage is 100%.
+  /**
+   * Calculates the VIP points percentage based on the given VIP points.
+   *
+   * @param vipPoints - The number of VIP points.
+   * @returns The VIP points percentage. The percentage is calculated as follows:
+   * - If vipPoints is 100 or less, the percentage is equal to vipPoints.
+   * - If vipPoints is between 101 and 200, the percentage is vipPoints minus 100.
+   * - If vipPoints is between 201 and 300, the percentage is vipPoints minus 200.
+   * - If vipPoints is greater than 300, the percentage is capped at 100.
+   */
   getVipPointsPercentage(vipPoints: number): number {
     if (vipPoints <= 100) {
       return vipPoints;
     } else if (vipPoints <= 200) {
-      return (vipPoints - 100);
+      return vipPoints - 100;
     } else if (vipPoints <= 300) {
-      return (vipPoints - 200);
+      return vipPoints - 200;
     } else {
       return 100;
     }
